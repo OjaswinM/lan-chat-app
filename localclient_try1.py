749150da65caa26bdabddb00ed29e2b0ebcc4b05
Feirydraco <majinsparks@gmail.com>
2018-02-23 21:59:13 +0530

    as

--- localclient_try1.py
+++ localclient_try1.py
@@ -2,9 +2,12 @@ import socket
 
 c = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-port = 11111
+port = 1111
 
 c.connect(('127.0.0.1',port))
 
 while(True):
-    print c.recv(1024)
+    message = raw_input('>')
+    c.send(message)
+    data = c.recv(4096)
+    print "Message from server: ", data
