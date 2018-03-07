3edd4a18af3f673b0c5e4912136bf10719046b93
ojaswin <ojaswin@hotmail.com>
2018-03-07 21:47:22 +0530

    Fixed broadcast

--- nClient.py
+++ nClient.py
@@ -4,7 +4,7 @@ def reciveFromServer(client_socket):
     while True:
         try:
             serverMessage = client_socket.recv(4096).decode()
-            print("Server message:", serverMessage)
+            print(serverMessage)
         except:
             pass
 
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     c = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
     host = 'localhost'
-    port = 1111
+    port = 12349
 
     c.connect((host, port))
 
