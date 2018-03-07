b279b97bed85fcd378e0d1753c582fadcf53f2aa
Feirydraco <majinsparks@gmail.com>
2018-03-07 18:57:59 +0530

    ,

---
+++ nClient.py
@@ -0,0 +1,27 @@
+import socket, threading
+
+def reciveFromServer(client_socket):
+    while True:
+        try:
+            serverMessage = client_socket.recv(4096).decode()
+            print("Server message:", serverMessage)
+        except:
+            pass
+
+if __name__ == '__main__':
+    c = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+    host = 'localhost'
+    port = 1111
+
+    c.connect((host, port))
+
+    fromServer = threading.Thread(target = reciveFromServer, args = (c, ))
+    fromServer.start()
+
+    while(True):
+        message = input('>')
+        c.send(message.encode())
+        if message == 'quit':
+            c.close()
+            break
--- nServer.py
+++ nServer.py
@@ -5,6 +5,7 @@ def acceptClient(server_socket):
         socket, address = server_socket.accept()
         connected.append((address, socket))
         print("<" + str(address[0]) + ":" + str(address[1]) + ">", "is now connected.")
+        socket.send("Welcome to the server!".encode())
         serve = threading.Thread(target = serveClient, args = (address, socket))
         serve.start()
 
@@ -13,10 +14,20 @@ def serveClient(address, client):
     while True:
         try:
             data = client.recv(4096).decode()
-            print("<" + str(address[0]) + ":" + str(address[1]) + ">", data)
+            if data == 'quit':
+                data = "is disconnecting.."
+                print("<" + str(address[0]) + ":" + str(address[1]) + ">", data)
+                break
+            else:
+                print("<" + str(address[0]) + ":" + str(address[1]) + ">", data)
+            # broadcast(client, data)
         except:
             pass
 
+def broadcast(currentClient, data):
+    for client in connected:
+        if currentClient is not client:
+            client[1].send(data.encode())
 
 
 if __name__ == '__main__':
@@ -34,6 +45,3 @@ if __name__ == '__main__':
 
     accept = threading.Thread(target = acceptClient, args = (s, ))
     accept.start()
-
-    # display = threading.Thread(target = printOnServer)
-    # display.start()
